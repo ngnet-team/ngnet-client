94f2caf2376b0235a119c172e8bb209ce7078ed9
Dimitar <Dimitar>
2021-08-27 12:13:09 +0300

    Add profile get request

--- Ngnet/src/app/auth/auth-routing.module.ts
+++ Ngnet/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -12,6 +13,11 @@ const routes: Routes = [
     path: 'register',
     pathMatch: 'full',
     component: RegisterComponent
+  },
+  {
+    path: 'profile',
+    pathMatch: 'full',
+    component: ProfileComponent
   }
 ];
 
---
+++ Ngnet/src/app/interfaces/user-response-model.ts
@@ -0,0 +1,8 @@
+export interface IUserResponseModel {
+    email?: string,
+    username?: string,
+    firstName?: string,
+    lastName?: string,
+    age?: number,
+    roleName?: string
+}
