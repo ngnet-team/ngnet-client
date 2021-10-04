c68057feda86bad328089f717bb19b4ed62cf9ef
Dimitar <Dimitar>
2021-10-04 18:56:27 +0300

    Add scss separation

--- Ngnet/src/app/shared/server-not-found/server-not-found.component.ts
+++ Ngnet/src/app/shared/server-not-found/server-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-not-found',
   templateUrl: './server-not-found.component.html',
-  styleUrls: ['./server-not-found.component.sass']
+  styleUrls: ['./server-not-found.component.scss']
 })
 export class ServerNotFoundComponent {
 
